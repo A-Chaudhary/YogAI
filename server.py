c2673d03d7ca0c8c662c30d98915b5a58090ffe6
Abhishek Chaudhary <62827856+A-Chaudhary@users.noreply.github.com>
2024-04-15 23:57:19 -0400

    index html setup

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ data = {
         "name": "Downward Dog Pose",
         "benefits": "Upper Body Strength: Activates shoulders, arms, and wrists for improved strength. Boosts Flexibility: Stretches hamstrings, calves, and foot arches, enhancing flexibility.",
         "mistakes": "Spinal Alignment: Avoid overarching the back to prevent spine compression. Shoulder Position: Keep shoulders away from ears to reduce neck and shoulder tension.",
-        "tips": "Hand Placement: Spread fingers and evenly press through palms and fingertips for balanced support. Hip Elevation: Aim for an inverted V-shape by lifting hips upwards, enhancing the pose’s effectiveness.",
+        "tips": "Hand Placement: Spread fingers and evenly press through palms and fingertips for balanced support. Hip Elevation: Aim for an inverted V-shape by lifting hips upwards, enhancing the pose's effectiveness.",
         "video": "Cil7tOPh4qM"
     }
 }
