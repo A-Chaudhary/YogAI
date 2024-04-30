eb66deb1accf1c4d79dc673a50e6ea22d618859d
jpeng15 <justinpeng15@gmail.com>
2024-04-30 16:24:18 -0400

    fixed video links

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ data = {
         "benefits": "Activates shoulders, arms, and wrists for improved strength. Stretches hamstrings, calves, and foot arches, enhancing flexibility.",
         "mistakes": "Avoid overarching the back to prevent spine compression. Keep shoulders away from ears to reduce neck and shoulder tension.",
         "tips": "Spread fingers and evenly press through palms and fingertips for balanced support. Aim for an inverted V-shape by lifting hips upwards, enhancing the pose’s effectiveness.",
-        "video": "Cil7tOPh4qM"
+        "video": "EC7RGJ975iM"
     },
     "1" : {
         "id": "1",
@@ -26,7 +26,7 @@ data = {
         "benefits": "Ideal for those who sit frequently, offering a deep stretch to alleviate tightness. Targets and fortifies inner thigh muscles, enhancing leg strength.",
         "mistakes": "Ensure the knee doesn’t collapse inward to avoid knee stress. Maintain level hips to prevent lower back and hip strain",
         "tips": "Activate your core to support the spine and enhance balance. Utilize blocks for hand or forearm support for easier reach.",
-        "video": "Cil7tOPh4qM"
+        "video": "jXk5dquBT6w"
     },
     "2" : {
         "id": "2",
@@ -36,7 +36,7 @@ data = {
         "benefits": "Mountain Pose helps in aligning the spine, shoulders, and hips, which improves overall posture. Practicing Tadasana increases body awareness, helping you identify misalignments and correct them.",
         "mistakes": "Letting the arches of the feet collapse instead of engaging them can lead to instability and improper alignment. Hyperextending or locking the knees can strain the joints. Instead, keep a micro-bend in the knees to maintain stability.",
         "tips": "Pay close attention to the alignment of your feet, ankles, knees, hips, shoulders, and head. Imagine yourself being pulled upward by a string from the crown of your head. Activate the muscles of the legs by lifting the kneecaps and engaging the quadriceps. Draw the belly button gently toward the spine to engage the core muscles.",
-        "video": "Cil7tOPh4qM"
+        "video": "Kv4C8CV8HjM"
     },
     "3" : {
         "id": "3",
@@ -46,7 +46,7 @@ data = {
         "benefits": "Warrior I strengthens the legs, particularly the quadriceps, hamstrings, and calves, helping to improve overall lower body strength. Practicing this pose challenges your balance, which helps to improve coordination and stability.",
         "mistakes": "Allowing the front knee to collapse inward can strain the knee joint. Ensure that the knee is stacked directly over the ankle. Placing the back foot at an awkward angle or not grounding it properly can lead to instability. The back foot should be turned out at a 45-degree angle and firmly rooted into the ground.",
         "tips": "Pay attention to the alignment of the feet, knees, hips, and shoulders. Maintain a strong foundation with proper alignment to maximize the benefits of the pose. Activate the muscles of the legs by pressing firmly into the ground with both feet. Engage the quadriceps and lift the kneecaps to support the stability of the pose.",
-        "video": "Cil7tOPh4qM"
+        "video": "NytDpa2r34g"
     },
     "4" : {
         "id": "4",
@@ -56,7 +56,7 @@ data = {
         "benefits": "Humble Warrior provides a deep stretch to the shoulders, chest, and upper back, helping to release tension in these areas. The pose stretches the hip flexors, groin, and quadriceps, promoting flexibility and mobility in the hips.elping to improve overall lower body strength. Practicing this pose challenges your balance, which helps to improve coordination and stability.",
         "mistakes": "Allowing the chest to collapse toward the thigh instead of maintaining an open chest can compromise the integrity of the pose and limit the stretch. Rounding the upper back instead of keeping it flat can strain the spine. Aim to keep the spine long and neutral.",
         "tips": "Pay attention to the alignment of the feet, knees, hips, and shoulders. Keep the front knee stacked directly over the ankle and the hips squared toward the front of the mat. Activate the muscles of the legs by pressing firmly into the ground with both feet. Engage the quadriceps and glutes to support the stability of the pose.",
-        "video": "Cil7tOPh4qM"
+        "video": "LwCWbc1y9sQ"
     },
     "5" : {
         "id": "5",
@@ -66,7 +66,7 @@ data = {
         "benefits": "Warrior II strengthens the legs, including the quadriceps, hamstrings, and calves, which promotes stability and balance. The pose opens up the hips, stretching the groin and hip flexors, which can alleviate tightness and improve flexibility.",
         "mistakes": "Allowing the front knee to collapse inward (toward the midline of the body) can strain the knee joint. Ensure the knee is stacked directly over the ankle. Incorrect placement of the back foot, such as pointing it too far forward or inward, can compromise stability and alignment. Aim to keep the back foot parallel to the short edge of the mat.",
         "tips": "Ensure the front heel is aligned with the arch of the back foot. This alignment helps maintain stability and supports proper hip positioning. Activate the core muscles by drawing the navel gently toward the spine. This action provides support to the lower back and helps maintain stability in the pose.",
-        "video": "Cil7tOPh4qM"
+        "video": "Mn6RSIRCV3w"
     },
     "6" : {
         "id": "6",
@@ -76,7 +76,7 @@ data = {
         "benefits": "Reverse Warrior stretches the side body, including the intercostal muscles, obliques, and latissimus dorsi, promoting increased flexibility and range of motion. The pose opens up the hips and groin, providing relief from tightness and improving hip mobility.",
         "mistakes": "Allowing the chest to collapse toward the thigh instead of maintaining an open chest can limit the stretch and compromise alignment. Excessively arching the lower back can strain the lumbar spine. Engage the core muscles and lengthen the tailbone toward the ground to maintain a neutral spine.",
         "tips": "Pay attention to the alignment of the feet, knees, hips, shoulders, and arms. Maintain a strong foundation with proper alignment to maximize the benefits of the pose. Activate the muscles of the legs by pressing firmly into the ground with both feet. Engage the quadriceps and inner thighs to support the stability of the pose.",
-        "video": "Cil7tOPh4qM"
+        "video": "xpKG_OrBWLI"
     },
     "7" : {
         "id": "7",
@@ -86,7 +86,7 @@ data = {
         "benefits": "Warrior III strengthens the muscles of the standing leg, including the quadriceps, hamstrings, and calves, as well as the core muscles, including the abdominals and lower back. Holding the pose challenges your balance and stability, helping to improve proprioception (awareness of body position in space) and coordination.",
         "mistakes": "Allowing the chest to collapse toward the ground can disrupt the alignment of the spine and compromise balance. Aim to keep the chest lifted and the spine long. Excessively arching the lower back can strain the lumbar spine. Engage the core muscles and lengthen the tailbone toward the extended heel to maintain a neutral spine.",
         "tips": "Place your hands on blocks or a chair for added support if you're working on building strength and stability in the pose. Activate the muscles of the core by drawing the navel gently toward the spine. This action helps stabilize the torso and maintain balance.",
-        "video": "Cil7tOPh4qM"
+        "video": "uEc5hrgIYx4"
     },
     "8" : {
         "id": "8",
@@ -96,7 +96,7 @@ data = {
         "benefits": "Plank pose primarily targets the muscles of the core, including the rectus abdominis, transverse abdominis, obliques, and lower back muscles. It helps strengthen these muscles, leading to improved stability and posture. While the core muscles are the primary focus, plank pose also engages muscles throughout the entire body, including the shoulders, arms, chest, and legs, promoting overall strength and endurance.",
         "mistakes": "Allowing the hips to sag toward the ground can put strain on the lower back and reduce the effectiveness of the pose. Keep the body in a straight line from head to heels by engaging the core muscles. Lifting the hips too high can create tension in the shoulders and limit engagement of the core muscles. Aim to maintain a neutral spine and avoid overarching or rounding the back.",
         "tips": "Focus on actively engaging the muscles of the core throughout the entire pose. Imagine drawing the navel toward the spine to create stability and support for the entire body. Pay attention to the alignment of the body, keeping the shoulders stacked over the wrists and the heels reaching back behind you. Avoid any sagging or lifting in the hips.",
-        "video": "Cil7tOPh4qM"
+        "video": "u6ZelKyUM6g"
     },
     "9" : {
         "id": "9",
@@ -106,7 +106,7 @@ data = {
         "benefits": "Boat Pose primarily targets the muscles of the abdomen, including the rectus abdominis, transverse abdominis, and obliques, helping to strengthen and tone the core. Holding Boat Pose requires balance and stability, which engages the muscles of the core, hips, and lower back, promoting overall stability and coordination.",
         "mistakes": "Allowing the spine to round or hunch forward can strain the lower back and reduce the effectiveness of the pose. Aim to keep the spine long and straight, with the chest lifted. Allowing the chest to collapse toward the thighs can disengage the core muscles and compromise balance. Keep the chest open and broad, lifting the sternum toward the ceiling.",
         "tips": "Focus on actively engaging the muscles of the core throughout the entire pose. Imagine drawing the navel toward the spine to create stability and support for the entire body. Pay attention to the alignment of the spine, keeping it long and straight from the tailbone to the crown of the head. Avoid rounding or overarching the back.",
-        "video": "Cil7tOPh4qM"
+        "video": "QVEINjrYUPU"
     },
     "10" : {
         "id": "10",
@@ -116,7 +116,7 @@ data = {
         "benefits": "Forearm Plank primarily targets the muscles of the core, including the rectus abdominis, transverse abdominis, and obliques, helping to strengthen and tone the entire midsection. Holding Forearm Plank engages the muscles of the shoulders, arms, and upper back, helping to build strength and stability in the upper body.",
         "mistakes": "Allowing the hips to sag toward the ground can put strain on the lower back and reduce the effectiveness of the pose. Keep the body in a straight line from head to heels by engaging the core muscles. Lifting the hips too high can create tension in the shoulders and limit engagement of the core muscles. Aim to maintain a neutral spine and avoid overarching or rounding the back.",
         "tips": "Focus on actively engaging the muscles of the core throughout the entire pose. Imagine drawing the navel toward the spine to create stability and support for the entire body. Pay attention to the alignment of the body, keeping the shoulders stacked directly over the elbows and the heels reaching back behind you. Avoid any sagging or lifting in the hips.",
-        "video": "Cil7tOPh4qM"
+        "video": "3QZlgJ40LfU"
     },
 }
 
